08ec1b69d790031709f5c43873c478fbce49e004
Paul Weaver <pauweave@cisco.com>
2013-05-18 16:26:18 +0100

    Renamed hidden files...
    now that there's an installer that copies them anyway.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-ln -s `pwd`/.prompt_command ~/.prompt_command
-ln -s `pwd`/.bash_colors~/.bash_colors
+ln -s `pwd`/prompt_command ~/.prompt_command
+ln -s `pwd`/bash_colors ~/.bash_colors
 
 echo "source ~/.bash_colors" >> ~/.bash_profile
 echo "source ~/.prompt_command" >> ~/.bash_profile
