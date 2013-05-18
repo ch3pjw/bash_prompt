c69279c598fd1179fd9075b02b5e8c5d26dc7d1b
Paul Weaver <pauweave@cisco.com>
2013-05-18 16:22:12 +0100

    Added a snippet of code that emualtes __git_ps1...
    that users can put in their bash_profile if necessary (like I have to do on my
    Mac)

---
+++ __git_ps1_snippet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This snippet of code is useful if you have bash installed but don't have the
+# __git_ps1 command available.
+
+function __git_ps1 {
+    branch_name=$(git branch 2>/dev/null | grep '*' | perl -pe 's/^\* //')
+    echo $(echo $1 | perl -pe "s/%s/${branch_name}/")
+}
