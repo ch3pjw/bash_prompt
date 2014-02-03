a2a0ba83bc69369481116986b3af8a27d1e10e4e
Liam Sharp <liasharp@cisco.com>
2014-02-03 16:02:51 +0000

    Quick fix to update .bash_rc as well as .bash_profile

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ ln -s `pwd`/bash_colors ~/.bash_colors
 echo "source ~/.bash_colors" >> ~/.bash_profile
 echo "source ~/.prompt_command" >> ~/.bash_profile
 echo 'export PROMPT_COMMAND="prompt_command"' >> ~/.bash_profile
+
+echo "source ~/.bash_colors" >> ~/.bash_rc
+echo "source ~/.prompt_command" >> ~/.bash_rc
+echo 'export PROMPT_COMMAND="prompt_command"' >> ~/.bash_rc
